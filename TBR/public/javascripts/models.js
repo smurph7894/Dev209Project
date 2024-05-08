7e9cf201803ae6b131760ae94acfe05024927267
Sarah <sarah.murphree06@gmail.com>
2024-05-07 18:50:10 -0700

    working on file structure

---
+++ TBR/public/javascripts/models.js
@@ -0,0 +1,32 @@
+const Book = function (pTitle, pAuthorID, pGenre, pSeries, pReadDate, pBookKeywords, pLocation){
+    this.id //random id
+    this.title = pTitle;
+    this.authorId = pAuthorID;
+    this.genre = pGenre;
+    this.series = pSeries;
+    this.readDate = pReadDate;
+    this.bookKeywords = pKeywords;
+    this.location = pLocation;
+};
+
+const Author = function(pFirstName, pLastName, pAlterEgos, pAuthorKeywords, pAuthorWebsite){
+    this.firstName = pFirstName;
+    this.lastName = pLastName;
+    this.alterEgos = pAlterEgos;
+    this.authorKeywords = pAuthorKeywords;
+    this.authorWebsite = pAuthorWebsite;
+};
+
+//make up data to use for front end that matches api
+//some things might changes once I build backend so check for discrepancies 
+
+
+
+// view main page
+// address - /books
+
+//view book page
+// address - /book/id
+
+//view author page
+// address - /book/:bookId/author
